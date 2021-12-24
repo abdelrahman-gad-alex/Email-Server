38f0863c13a9d9eac788089da2f715dae1a30c92
Mento79 <bodyomar79@gmail.com>
2021-12-24 18:37:59 +0200

    mail view correction

--- Front/src/app/mail-view/mail-view.component.ts
+++ Front/src/app/mail-view/mail-view.component.ts
@@ -12,7 +12,6 @@ export class MailViewComponent implements OnInit {
   to="";
   subject="";
   mails=MAILS
-  str='<h1 style="text-decoration: underline; text-align: center;"><em>adwadw</em></h1><ol><li><span style="text-decoration: underline;"><strong>Mento Is herree</strong></span></li><li><span style="text-decoration: underline;"><strong>adwhawfo</strong></span></li><li><span style="text-decoration: underline;"><strong>awfda</strong></span></li><li><span style="text-decoration: underline;"><strong>ewf</strong></span></li><li><span style="text-decoration: underline;"><strong>re</strong></span></li><li><span style="text-decoration: underline;"><strong>asf</strong></span></li><li><span style="text-decoration: underline;"><strong>ewfaef</strong></span></li><li><span style="text-decoration: underline;"><strong>wad</strong></span></li><li><span style="text-decoration: underline;"><strong>﻿﻿<br></strong></span></li></ol><p _ngcontent-vwx-c60=""></p>'
 
   constructor(private route:ActivatedRoute,private router: Router) {  
     
@@ -28,7 +27,7 @@ export class MailViewComponent implements OnInit {
     this.to=result[0].to
     this.subject=result[0].subject
     
-    document.getElementById("mailContent")!.innerHTML=this.str
+    document.getElementById("mailContent")!.innerHTML=result[0].mailContent
   }
 
   reply(){

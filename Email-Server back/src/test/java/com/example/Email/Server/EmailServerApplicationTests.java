78d41b7ffea91c0dc2f825fc42b580c83d6876be
abdelaziz <90736727+Abdelaziz25@users.noreply.github.com>
2021-12-29 20:06:07 +0200

    handling unit testing in back

--- Email-Server back/src/test/java/com/example/Email/Server/EmailServerApplicationTests.java
+++ Email-Server back/src/test/java/com/example/Email/Server/EmailServerApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.Email.Server;
 
+import com.example.Email.Server.Controller.MainController;
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import static org.junit.Assert.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EmailServerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws JSONException {
+		MainController test = new MainController();
+		String ans= test.login("abdelaiz@gmail.com","1234567");
+		assertEquals("invalid ",ans);
+		String ans2= test.Signup("{email:abdelaziz@gmail.com,password:1234567}");
+		assertEquals("user is added",ans2);
+
+
 	}
+
 
 }
---
+++ Email-Server back/src/test/java/com/example/Email/Server/fourthtest.java
@@ -0,0 +1,17 @@
+package com.example.Email.Server;
+import com.example.Email.Server.Controller.MainController;
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+public class fourthtest {
+    @Test
+    void contextLoads() throws JSONException {
+        MainController test = new MainController();
+        String ans2= test.editcontact("abdelaziz@gmail.com","zizo", "mento", new String[]{"[abdelaziz@gmail.com]"});
+        assertEquals("contact do not edited",ans2);
+    }
+}

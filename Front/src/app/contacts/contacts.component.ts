a700f98be2164da2203b192c4a4acd5303994ec9
Mento79 <bodyomar79@gmail.com>
2021-12-29 14:43:51 +0200

    mento

--- Front/src/app/contacts/contacts.component.ts
+++ Front/src/app/contacts/contacts.component.ts
@@ -12,6 +12,11 @@ import { SharedService } from '../shared/shared.service';
 })
 export class ContactsComponent implements OnInit {
 
+  foldText: string = ""
+  id:number[]=[]
+  keyText: string = ""
+  fileIn: string = ""
+  operation: string = ""
 
   faStar=faStar;
   faClock=faClock;
@@ -46,6 +51,228 @@ export class ContactsComponent implements OnInit {
   ngOnInit(): void {
 
   }
+
+  colParam!: string
+  searchClick(col: string)
+  {
+    this.keyText = ""
+    document.getElementById("textDiv2")!.style.display="none"
+    document.getElementById("butDiv")!.style.display="block"
+    document.getElementById("textDiv1")!.style.display="block"
+    this.operation = "search"
+    this.colParam = col
+    // this.operButClick()
+  }
+  moveClick()
+  {
+    this.foldText = ""
+    document.getElementById("textDiv1")!.style.display="none"
+    document.getElementById("butDiv")!.style.display="block"
+    document.getElementById("textDiv2")!.style.display="block"
+    this.operation = "move"
+  }
+  filterClick(col: string)
+  {
+    this.foldText = ""
+    this.keyText =""
+    document.getElementById("textDiv1")!.style.display="block"
+    document.getElementById("butDiv")!.style.display="block"
+    document.getElementById("textDiv2")!.style.display="block"
+    this.operation= "filter"
+  }
+  sortClick(col: string, order:string)
+  {
+    document.getElementById("textDiv1")!.style.display="none"
+    document.getElementById("butDiv")!.style.display="none"
+    document.getElementById("textDiv2")!.style.display="none"
+    let foldIdx = -1
+    for(let i = 0; i < this.contacts.length; i++)
+    {
+      if(this.fileIn == this.folder[i].name)
+      {
+        foldIdx = i
+        break
+      }
+    }
+    // this.folder[foldIdx].id 
+    this.http.get("http://localhost:8080/controller/sort",{
+      responseType:'text',
+      params:{
+          body: this.shared.getUser(),
+          foldr: this.fileIn,
+          method: col
+      },
+      observe:'response'
+    }).subscribe(
+      response =>{
+        this.res =JSON.parse(<string>response.body)
+        this.folder[foldIdx].id = this.res.ans
+        if(order == 'asc')
+        {
+          this.id = this.folder[foldIdx].id.reverse()
+        }
+        else
+        {
+          this.id = this.folder[foldIdx].id
+        }
+        this.mails = []
+        let MAILS = this.shared.getMails()
+        for(let i of this.id)
+          this.mails.push(MAILS[i])
+        this.dataSource = new MatTableDataSource<Mail>(this.mails);
+      }
+    )
+  }
+  operButClick()
+  {
+    let tempIdx : number = -1
+    let tempToFolder : number = -1
+    if(this.operation == "move" || this.operation=="delete")
+    {
+      let allMailsIdx = -1
+      let temp = this.foldText
+      if(this.operation == "delete")
+      {
+        temp = "trash"
+        console.log("marwan pablo")
+      }
+      if(temp == 'inbox' || temp == 'sent' || (temp == 'trash' && this.operation != "delete") || temp == 'draft' || temp == 'allMails')
+      {
+        alert("You can not move to this folder")
+        return
+      }
+      else if(this.fileIn == temp)
+      {
+        alert("You can not move to current folder")
+        return
+      }
+      else
+      {
+        // let flag: boolean = false
+        for(let i = 0; i < this.folder.length; i++)
+        {
+          if(this.folder[i].name == temp)
+          {
+            tempToFolder = i
+            // flag = true
+          }
+        }
+      }
+      if(tempToFolder == -1)
+      {
+        alert("File not found")
+        return
+      }
+      for(let i = 0; i < this.selectedMails.length; i++)
+      {
+        for(let j = 0; j < this.folder.length; j++)
+        {
+          if(this.folder[j].name == this.fileIn || (this.folder[j].name == "allMails" && this.operation=="delete"))
+          {
+            if(this.operation=="delete")
+            {
+              allMailsIdx = j
+              this.folder[j].id.splice(this.folder[j].id.indexOf(this.selectedMails[i]), 1)
+            }
+            tempIdx = j
+            this.folder[j].id.splice(this.folder[j].id.indexOf(this.selectedMails[i]), 1)
+          }
+          // else if(this.folder[j].name == temp)
+          // {
+          //   
+          // }
+        }
+      }
+      this.shared.setFolderID(this.fileIn, this.folder[tempIdx].id)
+      let tempArr = this.selectedMails.concat(this.folder[tempToFolder].id)
+      this.folder[tempToFolder].id = tempArr
+      if(this.operation == "delete")
+      {
+        this.shared.setFolderID("allMails", this.folder[allMailsIdx].id)
+      }
+      this.shared.setFolderID(temp, tempArr)
+      let MAILS = this.shared.getMails()
+      this.id = this.folder[tempIdx].id
+      this.mails = []
+      this.selectedMails = []
+      for(let i of this.id)
+        this.mails.push(MAILS[i])
+      this.dataSource = new MatTableDataSource<Mail>(this.mails);
+
+    }
+    else if(this.operation == "search")
+    {
+      if(this.keyText == "")
+      {
+        alert("Enter text to search for")
+      }
+      this.http.get("http://localhost:8888/controller/search",{
+      responseType:'text',
+      params:{
+          user: this.shared.getUser(),
+          folder: this.fileIn,
+          seatchBy: this.colParam,
+          equal: this.keyText
+      },
+      observe:'response'
+    }).subscribe(
+      response =>{
+        this.res =JSON.parse(<string>response.body)
+        this.id = this.res.ans
+        this.mails = []
+        let MAILS = this.shared.getMails()
+        for(let i of this.id)
+          this.mails.push(MAILS[i])
+        this.dataSource = new MatTableDataSource<Mail>(this.mails);
+        // this.folder[foldIdx].id = this.res.ans
+        // if(order == 'asc')
+        // {
+        //   this.id = this.folder[foldIdx].id.reverse()
+        // }
+        // else
+        // {
+        //   this.id = this.folder[foldIdx].id
+        // }
+        // this.mails = []
+        // let MAILS = this.shared.getMails()
+        // for(let i of this.id)
+        //   this.mails.push(MAILS[i])
+        // this.dataSource = new MatTableDataSource<Mail>(this.mails);
+      }
+    )
+    }
+    else if(this.operation == "filter")
+    {
+      if(this.keyText == "")
+      {
+        alert("Enter text to search for")
+        return
+      }
+      else if(this.foldText== "")
+      {
+        alert("Enter folder to move to")
+        return
+      }
+////////////////////////////////////////////////////
+      if(this.foldText=="")
+    alert("Please Enter A folder Name")
+    else{
+      let obj = this.shared.getFolders().find(f=>f.name==this.foldText);
+      
+      if (obj){
+        alert("There ara a folder with the same selected name . Please choose new name.")
+      }
+      else{
+        this.shared.getFolders().push(  {"name": this.foldText ,"id":[] }    )
+      this.foldText=""
+      }
+    }
+    console.log(this.shared.getFolders())
+    
+//////////////////////////////////////////////////////////////
+    } 
+  }
+
 
   sendMail(mail:String){
     this.router.navigate(['compose',mail])
